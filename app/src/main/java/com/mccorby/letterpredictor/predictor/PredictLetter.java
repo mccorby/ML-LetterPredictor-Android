da02c8d9b58d35c4fe84c8a2fbe90d5c49f27835
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2017-02-25 18:32:09 +0000

    Added Tensoflow interface

---
+++ app/src/main/java/com/mccorby/letterpredictor/predictor/PredictLetter.java
@@ -0,0 +1,8 @@
+package com.mccorby.letterpredictor.predictor;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+public class PredictLetter {
+
+    private TensorFlowInferenceInterface mTensorFlowInferenceInterface;
+}
